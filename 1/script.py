d757082f35ecb3ccc7279273be77f9a636d6065f
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2023-12-01 07:48:53 +0000

    [Day 1] re-add part 1

--- 1/script.py
+++ 1/script.py
@@ -1,7 +1,7 @@
 import sys
 
 if __name__ == '__main__':
-    inputs = (line.rstrip('\n') for line in open(sys.argv[1]))
+    inputs = [line.rstrip('\n') for line in open(sys.argv[1])]
 
     # that sure was some awkward bullshit for day 1
     m = {'one': 'o1ne',
@@ -14,6 +14,18 @@ if __name__ == '__main__':
         'eight': 'e8ight',
         'nine': 'n9ine'}
 
+    # p1
+    sum = 0
+    for i in inputs:
+        i = ''.join(filter(str.isdigit, i))
+
+        if i:
+            i = f'{i[0]}{i[-1]}'
+            sum += int(i)
+
+    print(sum)
+
+    # p2
     sum = 0
     for i in inputs:
         for word, replace in m.items():
