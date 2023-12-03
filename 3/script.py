89184943b1e83ab20879a5bcc198cb01c43d7a7f
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2023-12-03 06:44:20 +0000

    [D3] p2 solved

--- 3/script.py
+++ 3/script.py
@@ -20,6 +20,32 @@ def search(grid, x, y):
                 return True
     return False
 
+def search_gear(grid, x, y):
+    width = len(grid[0])
+    height = len(grid)
+
+    part_nums = []
+    for search_y in [-1,0,1]:
+        if y + search_y < 0 or y + search_y > height-1:
+            continue
+
+        line_part_nums = []
+
+        for search_x in [-1,0,1]:
+            if x + search_x < 0 or x + search_x > width-1:
+                continue
+            search = grid[y + search_y][x + search_x]
+            if isinstance(search, int) and search not in line_part_nums:
+                 line_part_nums.append(search)
+
+        part_nums.extend(line_part_nums)
+
+    if len(part_nums) == 2:
+        #print(part_nums, '->', part_nums[0] * part_nums[1])
+        return part_nums[0] * part_nums[1]
+    else:
+        #print(part_nums, 'X')
+        return 0
 
 if __name__ == '__main__':
     inputs = [line.rstrip('\n') for line in open(sys.argv[1])]
@@ -43,13 +69,13 @@ if __name__ == '__main__':
     parts = []
     for y in range(len(grid)):
         searching = True
-        line_parts = []
+        #line_parts = []
         for x in range(len(grid[y])):
             if isinstance(grid[y][x], int):
                 if searching:
                     if search(grid, x, y):
                         parts.append(grid[y][x])
-                        line_parts.append(grid[y][x])
+                        #line_parts.append(grid[y][x])
                         searching = False
             else:
                 searching = True
@@ -57,3 +83,13 @@ if __name__ == '__main__':
         #print(y, line_parts)
 
     print(sum(parts))
+
+    gear_sum = 0
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            if grid[y][x] != '*':
+                continue
+            
+            gear_sum += search_gear(grid, x, y)
+
+    print(gear_sum)
