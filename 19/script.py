efb76d4f441a67e31f22d927f7a234b94307491d
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2023-12-09 11:57:08 +0000

    [D19] p2 solved

--- 19/script.py
+++ 19/script.py
@@ -2,18 +2,38 @@ import sys
 from collections import defaultdict
 import re
 
+
 if __name__ == '__main__':
     replacements, molecule = open(sys.argv[1]).read().strip().split('\n\n')
 
-    conversions = defaultdict(list)
+    conversions = {}
     for replacement in replacements.split('\n'):
         start, end = replacement.split(' => ')
-        conversions[start].append(end)
+        conversions[end] = start
 
     unique_molecules = set()
-    for original, replacements in conversions.items():
+    for replacement, original in conversions.items():
         for token in re.finditer(original, molecule):
-            for replace in replacements:
-                new_molecule = molecule[:token.start()] + replace + molecule[token.end():]
-                unique_molecules.add(new_molecule)
+            new_molecule = molecule[:token.start()] + replacement + molecule[token.end():]
+            unique_molecules.add(new_molecule)
     print(len(unique_molecules))
+
+    def conversion_steps(molecule, steps=0):
+        if molecule == "e":
+            return steps
+
+        # generate all the possible molecules the current molecule could become
+        possible_steps = set()
+        for original, replacement in conversions.items():
+            for token in re.finditer(original, molecule):
+                possible_steps.add(molecule[:token.start()] + replacement + molecule[token.end():])
+
+        # sort so we check the largest length reductions first
+        possible_steps = sorted(sorted(possible_steps, reverse=True), key=len)
+
+        while possible_steps:
+            # recurse and try each of the steps until we find the right path to e
+            return conversion_steps(possible_steps.pop(0), steps + 1)
+        return None
+
+    print(conversion_steps(molecule))
