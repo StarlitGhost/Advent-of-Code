3ea8366e2ca4d477bec87cce9d518544e6a64e3c
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2023-12-09 06:58:34 +0000

    [D9] no need to append that 0

--- 9/script.py
+++ 9/script.py
@@ -9,7 +9,6 @@ if __name__ == '__main__':
         while any(e[-1]):
             e.append([second - first for first, second in zip(e[depth], e[depth][1:])])
             depth += 1
-        e[-1].append(0)
         while len(e) > 1:
             e[-2].append(e[-2][-1] + e[-1][-1])
             e.pop(-1)
