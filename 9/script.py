2ae398086f9fcce755d4d749c4654d231b73dae3
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2023-12-09 08:00:13 +0000

    [D9] rename variables to be clearer

--- 9/script.py
+++ 9/script.py
@@ -3,16 +3,17 @@ import sys
 if __name__ == '__main__':
     inputs = [line.rstrip('\n') for line in open(sys.argv[1])]
 
-    def extrapolate(values):
-        e = [values]
-        while any(e[-1]):
-            e.append([second - first for first, second in zip(e[-1], e[-1][1:])])
-        while len(e) > 1:
-            e[-2].append(e[-2][-1] + e[-1][-1])
-            e.pop(-1)
-        return e[-1][-1]
+    def extrapolate(sequence):
+        # d = derivative sequences
+        d = [sequence]
+        while any(d[-1]):
+            d.append([second - first for first, second in zip(d[-1], d[-1][1:])])
+        while len(d) > 1:
+            d[-2].append(d[-2][-1] + d[-1][-1])
+            d.pop(-1)
+        return d[-1][-1]
 
-    values = [list(map(int, line.split())) for line in inputs]
+    sequences = [list(map(int, line.split())) for line in inputs]
 
-    print(sum(extrapolate(v) for v in values))
-    print(sum(extrapolate(list(reversed(v))) for v in values))
+    print(sum(extrapolate(sequence) for sequence in sequences))
+    print(sum(extrapolate(list(reversed(sequence))) for sequence in sequences))
