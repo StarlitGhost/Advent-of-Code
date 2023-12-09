be3709b7dc228c57f8c667087eeb5eae222354a2
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2023-12-09 07:46:08 +0000

    [D9] no need to track depth, either

--- 9/script.py
+++ 9/script.py
@@ -5,10 +5,8 @@ if __name__ == '__main__':
 
     def extrapolate(values):
         e = [values]
-        depth = 0
         while any(e[-1]):
-            e.append([second - first for first, second in zip(e[depth], e[depth][1:])])
-            depth += 1
+            e.append([second - first for first, second in zip(e[-1], e[-1][1:])])
         while len(e) > 1:
             e[-2].append(e[-2][-1] + e[-1][-1])
             e.pop(-1)
