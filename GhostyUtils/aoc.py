68dfa19de83bef648b4e77728877efb0f633c3dc
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2023-12-16 19:58:35 +0000

    [aoc] add by_lines() generator input reader

--- GhostyUtils/aoc.py
+++ GhostyUtils/aoc.py
@@ -2,18 +2,31 @@ import sys
 from typing import Iterable
 
 
-def read(file: str = None) -> str:
+def _get_filename(file: str) -> str:
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+    else:
+        return 'input'
+
+
+def _generator(file: str = None):
     if file is None:
-        if len(sys.argv) > 1:
-            file = sys.argv[1]
-        else:
-            file = 'input'
-    return open(file).read().strip()
+        file = _get_filename(file)
+    return open(file)
 
 
-def read_lines(file: str = None) -> Iterable[str]:
+def read(file: str = None) -> str:
+    return _generator(file).read().strip()
+
+
+def read_lines(file: str = None) -> list[str]:
     return read(file).splitlines()
 
 
-def read_sections(file: str = None, split: str = '\n\n') -> Iterable[str]:
+def read_sections(file: str = None, split: str = '\n\n') -> list[str]:
     return read(file).split(split)
+
+
+def by_lines(file: str = None) -> Iterable[str]:
+    for line in _generator(file):
+        yield line
