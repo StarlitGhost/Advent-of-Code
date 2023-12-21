fcc4a1e0a8f9775970e29abd6c7b3bfded038c70
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2023-12-21 16:52:16 +0000

    [D19] p2 solved

--- 19/script.py
+++ 19/script.py
@@ -1,4 +1,9 @@
 from GhostyUtils import aoc
+import math
+
+
+MIN = 1
+MAX = 4000
 
 
 def process_workflow(workflow, part):
@@ -19,6 +24,48 @@ def process_workflow(workflow, part):
                     continue
         else:
             return rule
+
+
+def calc_ranges(name: str, workflows: dict, ranges: dict, accepted: list):
+    w_name = name.split('.')[-1]
+    # print(w_name, ranges)
+    for rule in workflows[w_name]:
+        if ':' in rule:
+            cmp, dest = rule.split(':')
+            if '<' in cmp:
+                prop, val = cmp.split('<')
+                tr = ranges.copy()
+                fr = ranges.copy()
+                tr[prop] = (tr[prop][0], int(val)-1)
+                fr[prop] = (int(val), fr[prop][1])
+            if '>' in cmp:
+                prop, val = cmp.split('>')
+                tr = ranges.copy()
+                fr = ranges.copy()
+                tr[prop] = (int(val)+1, tr[prop][1])
+                fr[prop] = (fr[prop][0], int(val))
+            if dest == 'A':
+                tr['combos'] = math.prod(v[1]-v[0]+1 for v in tr.values())
+                # print('A', tr)
+                accepted.append(tr)
+                ranges = fr
+                continue
+            if dest == 'R':
+                # print('R', tr)
+                ranges = fr
+                continue
+            calc_ranges(f'{name}.{dest}', workflows, tr, accepted)
+            ranges = fr
+        else:
+            if rule == 'A':
+                ranges['combos'] = math.prod(v[1]-v[0]+1 for v in ranges.values())
+                # print('A', ranges)
+                accepted.append(ranges)
+                continue
+            if rule == 'R':
+                # print('R', ranges)
+                continue
+            calc_ranges(f'{name}.{rule}', workflows, ranges, accepted)
 
 
 if __name__ == "__main__":
@@ -55,4 +102,10 @@ if __name__ == "__main__":
     for part in accepted:
         for prop in 'xmas':
             total += part[prop]
-    print(total)
+    print('p1', total)
+
+    ranges = {'x': (MIN, MAX), 'm': (MIN, MAX), 'a': (MIN, MAX), 's': (MIN, MAX)}
+    accepted = []
+    calc_ranges('in', workflows, ranges, accepted)
+    total = sum(a['combos'] for a in accepted)
+    print('p2', total)
