6bbe6396a2fcaf293d118809da16468239290efc
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2023-12-26 19:54:08 +0000

    [2019/9] p2 solved

--- 2019/9/script.py
+++ 2019/9/script.py
@@ -3,10 +3,17 @@ from GhostyUtils.intcode.cpu import IntCode
 
 
 def main():
+    inputs = [2, 1]
     output = []
-    cpu = IntCode(aoc.read(), input=[1].pop, output=output.append)
+    cpu = IntCode(aoc.read(), input=inputs.pop, output=output.append)
+
     cpu.process()
-    print(output)
+    print('p1:', output)
+    output.clear()
+
+    cpu.reset()
+    cpu.process()
+    print('p2:', output)
 
 
 if __name__ == "__main__":
