e73b11d36e39664c35a6e211665181bf18f23188
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2023-12-26 23:32:36 +0000

    [2019/10] p2 solved

--- 2019/10/script.py
+++ 2019/10/script.py
@@ -9,6 +9,8 @@ def main():
     asteroids = list(grid.find_all('#'))
 
     most_seen = 0
+    best_asteroid = None
+    best_angles = {}
     for asteroid in asteroids:
         angles = defaultdict(list)
         for other in asteroids:
@@ -21,8 +23,27 @@ def main():
 
         if len(angles) > most_seen:
             most_seen = len(angles)
+            best_asteroid = asteroid
+            best_angles = angles
 
-    print('p1:', most_seen)
+    print('p1:', most_seen, best_asteroid)
+
+    angles = sorted(best_angles.keys())
+    vaporised = 0
+    last_vaporised = None
+    empty = []
+    while vaporised != 200:
+        for angle in angles:
+            if best_angles[angle]:
+                last_vaporised = Vec2(best_angles[angle].pop(0))
+                vaporised += 1
+                if vaporised == 200:
+                    break
+                if not best_angles[angle]:
+                    empty.append(angle)
+        for angle in empty:
+            del best_angles[angle]
+    print('p2:', last_vaporised.x * 100 + last_vaporised.y, last_vaporised)
 
 
 if __name__ == "__main__":
