51e6d4055df6a94506fbe0127b39ccf9e31dd40d
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2024-12-03 22:02:11 +0000

    [2024/3] p2 solved

--- 2024/3/script.py
+++ 2024/3/script.py
@@ -1,11 +1,36 @@
 from GhostyUtils import aoc
 import re
+import math
+
+
+def find_muls(stream):
+    muls = [tuple(map(int, m[1].split(","))) for m in re.finditer(r"mul\((\d+,\d+)\)", stream)]
+    return muls
+
+
+def find_instrs(stream):
+    instrs = [m for m in re.finditer(r"mul\((\d+,\d+)\)|do\(\)|don't\(\)", stream)]
+    return instrs
 
 
 def main():
-    instrs = aoc.read()
-    muls = re.findall(r"mul\((\d+),(\d+)\)", instrs)
-    print("p1:", sum(int(m1) * int(m2) for m1, m2 in muls))
+    stream = aoc.read()
+
+    muls = find_muls(stream)
+    print("p1:", sum(l * r for l, r in muls))
+
+    enabled = True
+    total = 0
+    instrs = find_instrs(stream)
+    for instr in instrs:
+        if instr[0] == "don't()":
+            enabled = False
+        elif instr[0] == "do()":
+            enabled = True
+        elif enabled:
+            total += math.prod(map(int, instr[1].split(',')))
+
+    print("p2:", total)
 
 
 if __name__ == "__main__":
