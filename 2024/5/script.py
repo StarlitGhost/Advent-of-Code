e6809db75ce9444c598c4728dfd16c318c40e055
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2024-12-05 15:03:28 +0000

    [2024/5] p2 solved

--- 2024/5/script.py
+++ 2024/5/script.py
@@ -9,17 +9,29 @@ def validate(page_order, rule_map) -> bool:
     return True
 
 
+def find_middle(page_order, rule_map) -> int:
+    for page in page_order:
+        pages = rule_map[page].intersection(set(page_order))
+        if len(pages) == len(page_order)//2:
+            return page
+        # print(page_order, page, rule_map[page], pages)
+
+
 def main():
     rules, page_orders = aoc.read_sections()
     rules = [list(map(int, rule.split('|'))) for rule in rules.split('\n')]
     page_orders = [list(map(int, pages.split(','))) for pages in page_orders.split('\n')]
-    rule_map = defaultdict(list)
+    rule_map = defaultdict(set)
     for rule in rules:
-        rule_map[rule[0]].append(rule[1])
+        rule_map[rule[0]].add(rule[1])
 
     print("p1:", sum(page_order[len(page_order)//2]
                      for page_order in page_orders
                      if validate(page_order, rule_map)))
+
+    print("p2:", sum(find_middle(page_order, rule_map)
+                     for page_order in page_orders
+                     if not validate(page_order, rule_map)))
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -3,10 +3,10 @@ My solutions to the yearly Advents of Code
 
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  Advent of Code - 170/460 ⭐
+  Advent of Code - 171/460 ⭐
 </h1>
 <h1 align="center">
-  2024 - 9 ⭐ - Python
+  2024 - 10 ⭐ - Python
 </h1>
 <a href="2024/1/script.py">
   <img src=".aoc_tiles/tiles/2024/01.png" width="161px">
